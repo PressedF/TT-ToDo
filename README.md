f20448d6762165ed9ebe85a27c99be5b7a8e154f
PressedF <mimic.mak@gmail.com>
2021-03-28 00:22:55 +0400

    complited

---
+++ README.md
@@ -0,0 +1,8 @@
+# Техническое задание на создание ToDo - list
+___
+:white_check_mark: Поиск фильтрация заметок по словам
+:white_check_mark: Создание заметок
+:white_check_mark: Изменение заметки
+:white_check_mark: Удаление заметки
+___
+:white_check_mark: Хранение осуществляется через local storage
---
+++ hello.js
@@ -0,0 +1,210 @@
+const d = document;
+const queryButton = d.querySelector('.main__button');
+const fullForm = d.getElementById('formAllPage');
+const formToChange = d.getElementById('formToChange');
+
+const todoItemList = d.querySelector('.main__list');
+const createForm = d.getElementById('create');
+
+const itemDelete = d.querySelector('.item--delete');
+const searchForm = d.querySelector('.header__form-input');
+const fullFormClose = d.querySelectorAll('.full__form-close');
+ 
+const todoItemLists = d.querySelectorAll('.main__list');
+
+const titleForm = d.getElementById('title');
+const descriptionForm = d.getElementById('description');
+const searchBtn = d.getElementById('searchButton');
+
+const titleChange = d.getElementById('titleChange');
+const descriptionChange = d.getElementById('descriptionChange');
+const change = d.getElementById('change');
+
+queryButton.onclick = function(){
+    fullForm.classList.toggle('visible');
+}
+
+fullFormClose.forEach(t => {
+    t.addEventListener('click', () => {
+        if(fullForm.click)
+            fullForm.classList.remove('visible');
+
+        if(formToChange.click)
+            formToChange.classList.remove('visible');
+    })
+})
+
+
+
+
+function isEmpty(message){
+    return message.trim() == '';
+}
+
+function _borderTop(form, value){
+    return form.style.borderTop = value;
+}
+
+function _borderBottom(form, value){
+    return form.style.borderBottom = value;
+}
+
+function _border(form, value){
+    return form.style.border = value;
+}
+
+
+let todos = [];
+
+(function(){
+    function Todo(
+        title,
+        description
+    ){
+        this.id = Math.floor(Math.random() % 24 * 10000);
+        this.title = title;
+        this.description = description;
+    }
+
+    Todo.prototype = {
+        addTodo: function(title, description){
+            todos.push(new Todo(title, description));
+
+            this.setLocaleStorage(todos);
+        },
+
+        renderTodo: function(todo){
+            todoItemList.innerHTML = '';
+            todo.forEach((t, i) => {
+                if(t){
+                    const li = d.createElement('li');
+                    t.id = t.id + 1;
+                    li.setAttribute('key', t.id);
+                    li.className = 'main__list-item';
+                    li.innerHTML = `
+                        <span class="item--title">${t.title}</span>
+                        <span class="item--description">${t.description}</span>
+                        <span class="item--delete"></span>
+                    `;
+                    todoItemList.insertAdjacentElement('beforeend', li);
+                }
+            })
+        },
+
+        setLocaleStorage: function(todo){
+            window.localStorage.setItem('todo', JSON.stringify(todos));
+
+            this.renderTodo(todo);
+        },
+
+        getFromLocaleStorage: function(){
+            let item = window.localStorage.getItem('todo');
+                if(item){
+                    todos = JSON.parse(item);
+                    this.setLocaleStorage(todos);
+                }
+        },
+
+        deleteTodo: function(id){
+            let _confirm = confirm('Вы уверены?');
+            if(_confirm){
+                todos = todos.filter(t => {
+                    return t.id != id;
+                })
+
+                console.log(todos);
+                this.setLocaleStorage(todos);
+            }
+        },
+
+        filterTodo: function(){
+            let text = searchForm.value.toLowerCase();
+            let array = [...todos];
+            if(text.trim() != ''){
+                array = array.filter((t, i) => t.title.toLowerCase() === text.toLowerCase() || t.description.toLowerCase() === text.toLowerCase());
+            }
+            this.renderTodo(array);
+        },
+
+        
+        changeTodo: function(event){
+            let query = d.querySelectorAll('.' + event.className);
+
+            let title = titleChange;
+            let description = descriptionChange;
+
+            let target = event.closest('li').querySelectorAll('span');
+
+            title.value = target[0].innerText;
+            description.value = target[1].innerText;
+                    
+            change.addEventListener('click', () => {
+                if(!isEmpty(descriptionForm.value) && !isEmpty(titleForm.value)){
+                    if(formToChange.click)
+                        formToChange.classList.remove('visible');
+
+                    return;
+                }
+
+                target[0].innerText = title.value;
+                target[1].innerText = description.value;
+
+                for(let i = 0; i < query.length; i++){
+                    target = event.closest('li').querySelectorAll('span');
+                    console.log(event.closest('li').getAttribute('key'))
+                    if(todos[i].id == event.closest('li').getAttribute('key')){
+                        todos[i].title = target[0].innerText;
+                        todos[i].description = target[1].innerText;
+                        
+                        
+                        this.setLocaleStorage(todos);
+                        break;
+                    }
+                }
+
+                if(formToChange.click)
+                    formToChange.classList.remove('visible');
+            })
+        },
+    }
+
+        let todo = new Todo();
+        todo.getFromLocaleStorage();
+
+
+        createForm.onclick = function(){
+            _borderTop(titleForm, '1px solid #000');
+            _borderBottom(titleForm, '1px solid #000');
+            _borderTop(descriptionForm, '1px solid #000');
+            _borderBottom(descriptionForm, '1px solid #000');
+
+            if(isEmpty(descriptionForm.value) && isEmpty(titleForm.value)){
+                _borderTop(titleForm, '1px solid red');
+                _borderBottom(titleForm, '1px solid red');
+                _borderTop(descriptionForm, '1px solid red');
+                _borderBottom(descriptionForm, '1px solid red');
+
+                return;
+            }
+
+            todo.addTodo(titleForm.value, descriptionForm.value);
+            todo.setLocaleStorage(todos);
+        }
+
+        todoItemList.onclick = function(event){
+            if(event.target.classList.contains('item--delete')){
+                todo.deleteTodo(event.target.parentNode.getAttribute('key'));
+            } 
+            
+            if(event.target.classList.contains('main__list-item')){
+                formToChange.classList.toggle('visible');
+
+                todo.changeTodo(event.target);
+            }
+        }
+
+        searchBtn.onclick = function(){
+            todo.filterTodo();
+        }
+}());
+
